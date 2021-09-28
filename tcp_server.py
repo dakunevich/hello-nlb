aa1510fcc4623e4b456d0a75e5f355b1f1147295
Dmitry Akunevich <dzmitry@akunevich.com>
2021-09-28 18:47:14 +0300

    Check Ctrl+C functionality

--- tcp_server.py
+++ tcp_server.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 from datetime import datetime
-import socket
+import signal, socket, sys, time
 
+def handler(signum, frame):
+    """ Catch <ctll+c> signal for clean stop"""
+    print('\nGracefully shutdown...')
+    server.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 # bind the socket to the port 6789
 address = ('', 6789)
-max_size = 1000
+max_size = 4096 # 4 KiB
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(address)
 server.listen(1)
 
-print(datetime.now(), ' Starting the server.')
+print(datetime.now(), '--- Starting the server. ---')
 
 while True:
-    print('Waiting for a client to call.')
+    print(datetime.now(), 'Waiting for a client to call.')
     connection, addr = server.accept()
 
     try:
         # show who connected to us
-        print(datetime.now(), ' Connection from ', addr)
-
+        print(datetime.now(), 'Connection from ', addr)
+        
         while True:
-            data = connection.recv(max_size)
+            data = b''
+            part = connection.recv(max_size)
+            data += part
             if data:
                 # output received data
-                print("Data: %s" % data)
+                print(datetime.now(), "Data: %s" % data)
             else:
                 # no more data -- quit the loop
-                print("no data.")
+                print(datetime.now(), "--- no data. Bye! ---")
                 break
+    except KeyboardInterrupt:
+        print("^C received")
+        sys.exit(1)
     finally:
         # Clean up the connection
         connection.close()
